a4ad2bc331f681bc49042e577a465109fa5c1555
git-mhasan <mahadi2020dev@gmail.com>
2022-02-17 02:59:51 +0600

    javascript Calculation button ok.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-const incomeInput = document.getElementById("income-input");
-const foodCostInput = document.getElementById("food-cost-input");
-const rentCostInput = document.getElementById("rent-cost-input");
-const clothCostInput = document.getElementById("cloth-cost-input");
+// const incomeInput = getInputValue("income-input");
+// const foodCostInput = getInputValue("food-cost-input");
+// const rentCostInput = getInputValue("rent-cost-input");
+// const clothCostInput = getInputValue("cloth-cost-input");
 
 // function for getting the input field value in float point number.
 function getInputValue(idName) {
+    // const inputField = document.getElementById(idName);
     const inputFieldText = document.getElementById(idName).value;
     const inputValue = parseFloat(inputFieldText);
+    // inputField.value = '';
     return inputValue;
 }
 
@@ -21,11 +23,56 @@ function displayMoneyAmount(idName, amount) {
     document.getElementById(idName).innerText = amount;
 }
 
+//Calculate Button clicked event handelled.
 document.getElementById("calculate-btn").addEventListener("click", function () {
+    const incomeInputValue = getInputValue("income-input");
+    const foodCostInputValue = getInputValue("food-cost-input");
+    const rentCostInputValue = getInputValue("rent-cost-input");
+    const clothCostInputValue = getInputValue("cloth-cost-input")
+
+    if (incomeInputValue >= 0 && foodCostInputValue >= 0 && rentCostInputValue >= 0 && clothCostInputValue >= 0) {
+        const totalExpenses = totalExpense(foodCostInputValue, rentCostInputValue, clothCostInputValue);
+        const balanceAmount = incomeInputValue - totalExpenses;
+        if (balanceAmount < 0) {
+            alert('\nOpps..!\nYour Expenses exceeds your Income.');
+        } else {
+            displayMoneyAmount("total-expense", totalExpenses);
+            displayMoneyAmount("balance", balanceAmount);
+        }
+    }
+    else if (isNaN(incomeInputValue) || incomeInputValue < 0) {
+        if (incomeInputValue < 0) {
+            alert('\n Please insert only positive amount as Income.');
+        } else {
+            alert('\n Please do not insert any text as Income.');
+        }
+    }
+    else if (isNaN(foodCostInputValue) || foodCostInputValue < 0) {
+        if (foodCostInputValue < 0) {
+            alert('\n Food expenses must be a non negative amount.');
+        } else {
+            alert('\n Please do not insert any text as Food Expense.');
+        }
+    }
+    else if (isNaN(rentCostInputValue) || rentCostInputValue < 0) {
+
+        if (rentCostInputValue < 0) {
+            alert('\n Rent should be a positive amount.');
+        } else {
+            alert('\n Please do not insert any text as Rent.');
+        }
+    }
+    else if (isNaN(clothCostInputValue) || clothCostInputValue < 0) {
+        if (clothCostInputValue < 0) {
+            alert('\n Please insert only positive amount for clothing expense.');
+        } else {
+            alert('\n Please do not insert any text as Clothing Expense.');
+        }
+    }
     // console.log(incomeInput.value, foodCostInput.value, rentCostInput.value, clothCostInput.value);
     // console.log(getInputValue("income-input"));
-    displayMoneyAmount("total-expense", 3444);
-    displayMoneyAmount("balance", 3884);
-    displayMoneyAmount("saving-amount", 3444);
-    displayMoneyAmount("remaining-balance", 3444);
+    // displayMoneyAmount("total-expense", 3444);
+    // displayMoneyAmount("balance", 3884);
+    // displayMoneyAmount("saving-amount", 3444);
+    // displayMoneyAmount("remaining-balance", 3444);
 })
